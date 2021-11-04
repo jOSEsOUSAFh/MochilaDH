1b0e5061704888ea88159b3caea2bb9c0e118096
“José <josesousacruzfh@outlook.com>
2021-11-03 22:16:58 -0300

    Atualizações apos aula 08

---
+++ ProgramaçãoImperativa/Aula08/condicionais.js
@@ -0,0 +1,9 @@
+let dia = "Quarta"
+
+if(dia == "Domingo"){
+    console.log("Vou para praia!")
+}else if(dia == "Sabado"){
+    console.log ("Vou para o cinema!")
+}else{
+ console.log ("Vou estudar!")  
+}
