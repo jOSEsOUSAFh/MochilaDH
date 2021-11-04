1f922b2c634df302a9d6cf1b12ca3a1dd5b4ddd3
“José <josesousacruzfh@outlook.com>
2021-11-03 22:35:47 -0300

    att

--- ProgramaçãoImperativa/Aula06/Calculadora.js
+++ ProgramaçãoImperativa/Aula06/Calculadora.js
@@ -15,5 +15,23 @@ console.log(Divisão(3,5))
 const Divpor0 = (ValorA) =>  0 / ValorA
 console.log(Divpor0(3))
 
-const quadradoDoNumero = ValorA => multiplica(ValorA,ValorA) * multiplica(ValorA,ValorA);
+const quadradoDoNumero = ValorA => multiplica(ValorA,ValorA);
 console.log(quadradoDoNumero(2))
+
+const mediaDeTresN = (ValorA, ValorB, ValorC) => {const total = soma(ValorA,ValorB + ValorC) /3;
+ return total.toFixed(2).replace('.',',')   
+
+}
+console.log(mediaDeTresN(3,5,6));
+
+const calculaPorcentagem = (ValorA, ValorB) => {
+    const total1 = ValorA * Divisão(ValorB,100)
+    return total1
+}
+console.log(calculaPorcentagem(160,25));
+
+
+
+const geradordePorcentagem = (ValorA, ValorB) => ValorA * 100 / ValorB
+console.log(geradordePorcentagem(2,200));
+
