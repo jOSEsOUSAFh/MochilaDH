a2a77f2b95059848efbefa270b28d991c63038bc
“José <josesousacruzfh@outlook.com>
2021-11-08 18:39:24 -0200

    Check-Point
    Primeiro Check-Point de Front-end

--- ProgramaçãoImperativa/Aula08/condicionais.js
+++
@@ -1,9 +0,0 @@
-let dia = "Quarta"
-
-if(dia == "Domingo"){
-    console.log("Vou para praia!")
-}else if(dia == "Sabado"){
-    console.log ("Vou para o cinema!")
-}else{
- console.log ("Vou estudar!")  
-}
